0c5d3156eac369f258695579319e5451e078aad8
platix37 <platoncristian97@gmail.com>
2016-12-12 22:20:09 +0200

    Add files via upload

---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    class Program
+    {
+        private static void Reverse()
+        {
+            //reverse fara vectori aditionali
+
+            int n, i, aux;
+            Console.Write("n= ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] v = new int[n];
+
+            for (i = 0; i < n; i++)
+                v[i] = i;
+
+            Console.WriteLine("Vectorul initial: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", v[i]);
+            }
+
+            for(i=0; i<n; i++)
+            {
+                aux = v[n - i-1];
+                v[n - i-1] = v[i];
+                v[i] = aux;
+                if (i >= n / 2)
+                    break;
+            }
+
+            Console.WriteLine("Vectorul final: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", v[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Reverse();
+        }
+    }
+}
